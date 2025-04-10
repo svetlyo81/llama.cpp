8078ff93ca2d4f598b802e61cd87a659da0971c7
svetlyo81 <>
2025-04-10 21:16:09 +0300

    vulkan - select discrete gpu with most vram (not tested)

--- ggml/include/ggml-vulkan.h
+++ ggml/include/ggml-vulkan.h
@@ -20,6 +20,7 @@ GGML_API GGML_CALL int  ggml_backend_vk_get_device_count(void);
 GGML_API GGML_CALL void ggml_backend_vk_get_device_description(int device, char * description, size_t description_size);
 GGML_API GGML_CALL void ggml_backend_vk_get_device_memory(int device, size_t * free, size_t * total);
 GGML_API GGML_CALL int ggml_backend_vk_get_device_type(int device);
+GGML_API GGML_CALL int ggml_backend_vk_get_device_index(void);
 
 GGML_API GGML_CALL ggml_backend_buffer_type_t ggml_backend_vk_buffer_type(size_t dev_num);
 // pinned host buffer for use with the CPU backend for faster copies between CPU and GPU
